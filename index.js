3578fd03773fa6646e530e76fe29b2c22656c225
Aourin <thetitan8706@gmail.com>
2016-09-24 11:48:15 -0400

    chore(setup): initial koa app with todo mock endpoint

---
+++ index.js
@@ -0,0 +1,37 @@
+import koa     from 'koa';
+import router  from 'koa-route';
+import json    from 'koa-json';
+
+const app = koa();
+
+//  Middleware
+app.use(json());
+
+//  Mock Todos
+const todos = {
+  list: function *() {
+    this.body = [
+      {
+        title: 'Willy\'s Todo',
+        user: {
+          email: 'fishmoo@fishcheesemoo.com',
+          name: 'Whacky Willy'
+        }
+      },
+      {
+        title: 'Off To The Mines',
+        user: {
+          email: 'miningguy@fishcheesemoo.com',
+          name: 'Mine Guy'
+        }
+      }
+    ];
+  }
+};
+
+//  Use Router
+app.use(
+  router.get('/todos', todos.list)
+);
+
+app.listen(3000);
