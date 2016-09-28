58bb1a86bd3b5c63a2cfc91298d8e51d5d76bb39
Aourin <thetitan8706@gmail.com>
2016-09-27 20:14:37 -0400

    feat(Todos): adds create endpoint

--- todos/list/create.js
+++ todos/list/create.js
@@ -1,7 +1,10 @@
+import TodoModel from '../model';
 /**
  * Fetch a list of todo items
  */
 export default function *create() {
-
-  this.body = {message: 'Success'};
+  const newTodo = yield TodoModel.create(this.request.body);
+  this.body = newTodo;
 }
+
+//TODO: Validate the todo item to be created
