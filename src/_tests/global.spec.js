eeaa3066578730ff5dde199ad2463467e95e7a06
Aourin <thetitan8706@gmail.com>
2016-09-27 22:27:38 -0400

    tests(global): adds sinon chai and proxyquire

--- src/_tests/global.spec.js
+++ src/_tests/global.spec.js
@@ -1,6 +1,16 @@
-import chai from 'chai';
+import chai       from 'chai';
+import sinon      from 'sinon';
+import sinonChai  from 'sinon-chai';
+import mock       from 'proxyquire';
 
+require('mocha-sinon')();
+require('mocha-generators').install();
+
+chai.use(sinonChai);
 chai.config.includeStack = true;
+
+// global.mockRequire = mock;
+global.sinon = sinon;
 global.expect = chai.expect;
 global.AssertionError = chai.AssertionError;
 global.Assertion = chai.Assertion;
